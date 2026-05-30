350a6d4f5b87b8471361e23953fc6bac83bccd24
ben <bholsinger@gmail.com>
2026-05-30 08:47:13 -0700

    Initial import of my project into SellerConnect

---
+++ SellerConnect/SellerConnect/SellerConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SellerConnectApp.swift
+//  SellerConnect
+//
+//  Created by Ben H on 5/30/26.
+//
+
+import SwiftUI
+
+@main
+struct SellerConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SellerConnect/SellerConnectTests/SellerConnectTests.swift
@@ -0,0 +1,19 @@
+//
+//  SellerConnectTests.swift
+//  SellerConnectTests
+//
+//  Created by Ben H on 5/30/26.
+//
+
+import Testing
+@testable import SellerConnect
+
+struct SellerConnectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
